ce2a89da328edfc1c848619f3f2256e3013fcf70
Tom Porter <tom.port@yahoo.co.uk>
2022-10-07 14:53:19 +0100

    Testing seems to now be working

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,4 +8,25 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         return new Array(0);
     }
+
+
+
+
+
+    /*movePawn(board, newSquare) {
+        let pawnSquare = currentSquare[row+1][col]
+        const currentSquare = board.findPiece(this);
+        board.movePiece(currentSquare, newSquare);
+    }
+    */
+    movePawn(board) {
+        if (this.Player === WHITE) {
+        let pawnSquare = [currentSquare[row+1][col]]
+        this.moveTo(board, pawnSquare)
+        }
+        else {let pawnSquare = [currentSquare[row-1][col]]
+        this.moveTo(board, pawnSquare)
+        }
+    }
 }
+
