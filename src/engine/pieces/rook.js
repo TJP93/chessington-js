aed24c3a87656bb15f8116acc2482bc9d8e930fb
Tom Porter <tom.port@yahoo.co.uk>
2022-10-10 12:02:00 +0100

    Rook passing tests

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Rook extends Piece {
@@ -6,6 +7,29 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this); // find location of the piece
+
+        const rookMovement = [];
+
+        for (let row = location.row + 1; row < 8; row++ ) {
+            let destination = new Square(row, location.col)   // moving up
+            rookMovement.push(destination);
+        }
+
+        for (let row = location.row - 1; row > -1; row--) {
+            let destination = new Square(row, location.col)  // moving down
+            rookMovement.push(destination);
+        }
+
+        for (let col = location.col + 1; col < 8; col++) {
+            let destination = new Square(location.row, col)  // moving right
+            rookMovement.push(destination);
+        }
+
+        for (let col = location.col - 1; col > - 1; col--) {
+            let destination = new Square(location.row, col)  // moving left
+            rookMovement.push(destination);
+        }
+        return rookMovement;
     }
 }
