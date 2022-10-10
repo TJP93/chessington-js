44ff1c210d2e5eb6c26c715e4ce0e9fa06a25c72
Tom Porter <tom.port@yahoo.co.uk>
2022-10-10 12:30:04 +0100

    NE/SE/SW/NE nested loops, Square not currently defined?

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -8,7 +8,35 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         const location = board.findPiece(this);
 
-        const bishopMovement = [];
+        const bishopMovement = []; // need to be able to move diagonally in all 4 directions. NE, SE, SW, NW. Both col and row increasing/decreasing simultaneously. Nested for loop?
+
+        for (let row = location.row + 1; row < 8; row++) {
+            for (let col = location.col + 1; col < 8; col++) {
+                let destination = new Square(row, col);                             // moving NE
+                bishopMovement.push(destination);
+            }
+        }
+
+        for (let row = location.row + 1; row < 8; row++) {
+            for (let col = location.col - 1; col > -1; col--) {
+                let destination = new Square(row, col);                             // moving SE
+                bishopMovement.push(destination);
+            }
+        }
+
+        for (let row = location.row - 1; row > -1; row--) {
+            for (let col = location.col - 1; col > -1; col--) {
+                let destination = new Square(row, col);                             // moving SW
+                bishopMovement.push(destination);
+            }
+        }
+
+        for (let row = location.row - 1; row > -1; row--) {
+            for (let col = location.col + 1; col < 8; col++) {
+                let destination = new Square(row, col);                             // moving NW
+                bishopMovement.push(destination);
+            }
+        }
 
 
         return bishopMovement;
