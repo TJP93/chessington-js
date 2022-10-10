9cad3245e458bed0bea7876c033c02ec6d1d8bf3
Tom Porter <tom.port@yahoo.co.uk>
2022-10-10 10:54:53 +0100

    Dave's pawn & piece solutions copied in and noted

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,31 +1,23 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
-        super(player);
+        super(player);  // call parent's initialiser
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
-    }
+        const location = board.findPiece(this); // find location of the piece
+        const delta = this.player === Player.WHITE ? 1 : -1; // determining position/movement of player. White 1, black -1.
+        
+        const singleMoveSquare = Square.at(location.row + delta, location.col); 
+        const doubleMoveSquare = Square.at(location.row + 2 * delta, location.col);
 
-    function(movePawn) {
-    const currentSquare = board.findPiece(this);
-    const colour = this.player;
-    let moves = new Array();
-    
-    if (colour === player.WHITE) {
-
-    let targetRow = currentSquare.row;
-    targetRow++;
-    let targetColumn = currentSquare.col;
-
-    let position = new Square(targetRow, targetColumn)
-
-    moves.push(position);
+        if (this.hasMoved) {
+            return [singleMoveSquare];    // if already moved, only does single move. Checked via hasMoved in piece.
+        } else {
+            return [singleMoveSquare, doubleMoveSquare];
+        }
     }
 }
-
-
-}
-
